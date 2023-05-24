83d4b541ddb63f344a08df8d6ebb1be1a888d32d
Xavier Challe <xchalle@student.42.fr>
2023-05-24 18:42:18 -0400

    add lvl8

---
+++ level8/level8.c
@@ -0,0 +1,14 @@
+printf("%p, %p \n", var_ab0, var_aac);
+fgets(var_s20, 128 , stdin);
+if (var_s20 == NULL)
+{
+	exit(0);
+}
+i = 5
+while (var_s20[i] == "auth "[i] && i>0)
+{
+	i--;
+	dl = "auth "[i] > var_s20[i];
+	al = "auth "[i] < var_s20[i];
+}
+al = al(25) - i;
